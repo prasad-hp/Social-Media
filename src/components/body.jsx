e9cd0c6eb772e72b7b9e23fd89c2e9d7b724cf46
Prasad Hp <prasadhp@outlook.in>
2024-04-14 22:31:39 +0530

    Created a dummy database and started the map funtction.

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ImageCard from "./ImageCard";
+import userData from "../data";
 
 
 function Body(){
+
     return (
         <div className="body">
             <ImageCard />
+            {userData.map((post)=>{
+                <ImageCard 
+                   name={userData[0].name}
+                   userName={userData[0].userName}
+
+                />
+            })}
         </div>
     )
 }
---
+++ src/data.jsx
@@ -0,0 +1,33 @@
+let userData = [{
+    name:"Prasad Hp",
+    userName:"prasadhp",
+    posts: [
+        {
+            id:"",
+            photo:"",
+            likes:"",
+            comments:"" },
+        {
+            id:"",
+            photo:"",
+            likes:"",
+            comments:"" }
+    ]
+},{
+    name:"Ramachandra",
+    userName:"chandu",
+    posts: [
+        {
+            id:"",
+            photo:"",
+            likes:"",
+            comments:"" },
+        {
+            id:"",
+            photo:"",
+            likes:"",
+            comments:"" }
+    ]
+}]
+
+export default userData;
