20b858bb43819f905f0bbf5a10ab275ee84e19ec
Prasad Hp <prasadhp@outlook.in>
2024-04-30 12:09:19 +0530

    updated the css for top part of the Post Card

--- src/components/ImageHeading.jsx
+++ src/components/ImageHeading.jsx
@@ -4,7 +4,7 @@ function ImageHeading(props){
     return(
         <div className="flex items-center justify-start w-full p-2">  
             <img src={`../../images/profile-photos/${props.userPost.userName}.jpg`} className="rounded-full w-12"/>
-            <h3 className="">{props.userPost.name}</h3>
+            <h3 className="font-semibold ml-3">{props.userPost.name}</h3>
         </div>
     )
 }
