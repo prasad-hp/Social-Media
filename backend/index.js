244519d0e8d2b6ca372a2b9f7116987aed55baac
Prasad Hp <prasadhp@outlook.in>
2024-04-30 20:06:00 +0530

    Initialized a basic express backed with get reqest

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
 import express from "express"
+const app = express()
+
+const port = 3000;
+
+app.get("/", (req, res)=>{
+    res.json("hello")
+})
+
+app.listen(port)
+console.log("port running successfully at" + port)
